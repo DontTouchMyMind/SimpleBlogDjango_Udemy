d76a7c5a35fd2e0723735375cbb7de08ffd30108
DontTouchMyMind <tobigface@gmail.com>
2020-09-21 11:29:56 +0200

    Fixed class Post.

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,11 @@
+from datetime import datetime
+
 from django.db import models
+
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    date = models.DateTimeField
-    text = models.TextField
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    text = models.TextField()
     image = models.ImageField(upload_to='post_images/')
