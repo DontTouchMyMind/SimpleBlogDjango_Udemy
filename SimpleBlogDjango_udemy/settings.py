ce572e486ffea085aaf59ada0ceeae8c8eb45e77
DontTouchMyMind <tobigface@gmail.com>
2020-09-21 11:01:30 +0200

    Added connection to PostgreSQL data base.

--- SimpleBlogDjango_udemy/settings.py
+++ SimpleBlogDjango_udemy/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'SimpleBlogDjango_udemy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testdb',
+        'USER': 'postgres',
+        'PASSWORD': '+',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
